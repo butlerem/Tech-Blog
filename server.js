598a76c03c829296406fd94e331435a249c5a769
Emma Butler <130527417+butlerem@users.noreply.github.com>
2023-08-29 12:05:02 -0400

    Revised Routs, Reinstalled Handlebars

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const sequelize = require('./config/connection'); // Moved to the top for visibi
 const apiRoutes = require('./controllers/apiRoutes');
 const homeRoutes = require('./controllers/homeRoutes');
 
+const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const apiRoutes = require('./controllers/apiRoutes');
+const homeRoutes = require('./controllers/homeRoutes');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,8 +21,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/', apiRoutes);
-app.use('/', homeRoutes);
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
@@ -36,7 +43,7 @@ app.use('/', homeRoutes);
 
 // 404 handler - should be almost last
 app.use((req, res) => {
-    res.status(404).end();
+    res.status(404).send('Page not found'); // A little more informative than .end()
 });
 
 // Start server after DB connection is established
